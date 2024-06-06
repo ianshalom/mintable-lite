40360aa1e5080523e1e59a58672456e7e7a05afa
Ian <ian@bambu.co>
2024-06-06 12:23:31 +0800

    feat: add login button

--- app/TopNav.tsx
+++ app/TopNav.tsx
@@ -5,9 +5,17 @@ export default function TopNav() {
   return (
     <nav className="fixed top-0 left-0 w-full h-16 border-b z-50 flex items-center bg-white">
       <div className="w-full px-6 md:p-0 md:w-4/6 mx-auto font-bold text-lg hover:cursor-pointer">
-        <Link href="/">
-          <p>MintableLite</p>
-        </Link>
+        <div className="flex justify-between items-center">
+          <Link href="/">
+            <p>MintableLite</p>
+          </Link>
+          <Link
+            className="border-2 border-black rounded-md py-1 px-4 hover:bg-gray-100 flex items-center justify-center"
+            href="/"
+          >
+            Log in
+          </Link>
+        </div>
       </div>
     </nav>
   );
